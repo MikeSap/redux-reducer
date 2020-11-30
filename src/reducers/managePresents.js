4d1ab221c3ca1d0d11cb1d5cef91514798a9e27d
mike <mikesapienzaaudio@gmail.com>
2020-11-30 10:45:07 -0600

    finished

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,13 @@
 export function managePresents(state, action){
+    
+    switch(action.type){    
+        case ("INCREASE"):
+            let currentNumOfPres = parseInt(state.numberOfPresents)
+            return {
+                numberOfPresents: currentNumOfPres + 1
+            }            
+    
+    default:
+        return state
+    }
 }
